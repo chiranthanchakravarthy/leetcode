167b56b4885d0330c86709185611db2cebcf0176
chiranthanchakravarthy <119587030+chiranthanchakravarthy@users.noreply.github.com>
2025-04-06 22:35:13 +0530

    Add files via upload

--- Daily question/jan 2025/6-4-25.py
+++ Daily question/jan 2025/6-4-25.py
@@ -1,6 +1,11 @@
 class Solution:
-    def subsetXORSum(self, nums):
-        total = 0
-        for num in nums:
-            total |= num  # Step 1: Compute bitwise OR of all numbers
-        return total * (1 << (len(nums) - 1))  # Step 2: Multiply by 2^(n-1)
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        dp = [[num] for num in nums]
+
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 and len(dp[j]) + 1 > len(dp[i]):
+                    dp[i] = dp[j] + [nums[i]]
+
+        return max(dp, key=len)s
